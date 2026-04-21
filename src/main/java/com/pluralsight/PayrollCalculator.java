e75fd357f4f047a01eac6702ee4370459abec67c
josem71411-gif <josem71411@gmail.com>
2026-04-21 15:00:03 -0500

    created the improved PayrollCalculator

--- src/main/java/com/pluralsight/PayrollCalculator.java
+++ src/main/java/com/pluralsight/PayrollCalculator.java
@@ -19,25 +19,57 @@ public class PayrollCalculator {
             BufferedReader reader = new BufferedReader(new FileReader(inputFile));
             PrintWriter writer = new PrintWriter(new FileWriter(outputFile));
 
-
-            reader.readLine();
             String line;
+
+            boolean isJson = outputFile.toLowerCase().endsWith(".json");
+
+            if (isJson) {
+                writer.println("[");
+            }
+            boolean first = true;
 
             while ((line = reader.readLine()) != null) {
 
+                System.out.println("READING: "+line);
+
                 String[] tokens = line.split("\\|");
+
+                if (tokens[0].equalsIgnoreCase("id")) continue;
 
                 int id = Integer.parseInt(tokens[0]);
                 String name = tokens[1];
                 double hoursWorked = Double.parseDouble(tokens[2]);
                 double payRate = Double.parseDouble(tokens[3]);
+
                 Employee emp = new Employee(id, name, hoursWorked, payRate);
-                System.out.printf("ID: %d  |  Name: %s  |  Gross Pay: $%.2f%n",
-                        emp.getEmployeeId(),
-                        emp.getName(),
-                        emp.getGrossPay());
+
+                System.out.println("WRITING: " + name);
+
+                if (isJson) {
+                    if (!first) {
+                        writer.println(",");
+                    }
+                    writer.printf("{ \"id\": %d, \"name\": \"%s\",\"grossPay\": %.2f }",
+                    emp.getEmployeeId(),
+                    emp.getName(),
+                    emp.getGrossPay());
+
+                first = false;
+                } else {
+                    writer.printf("%d|%s|%.2f%n",
+                    emp.getEmployeeId(),
+                    emp.getName(),
+                    emp.getGrossPay());
+                }
             }
+            if (isJson) {
+                writer.println();
+                writer.println("]");
+            }
+            writer.close();
             reader.close();
+
+            System.out.println("Payroll file created successfully!");
         } catch (IOException e) {
             System.out.println("Error reading file: " + e.getMessage());
         }
