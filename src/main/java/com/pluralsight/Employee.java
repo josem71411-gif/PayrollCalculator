17d3e1efa1ff9ebbee85539de1002d48252b88ca
josem71411-gif <josem71411@gmail.com>
2026-04-21 13:45:33 -0500

    Created the employee class, and created the constructors

---
+++ src/main/java/com/pluralsight/Employee.java
@@ -0,0 +1,20 @@
+package com.pluralsight;
+
+public class Employee {
+    private int employeeID;
+    private String name;
+    private double hoursWorked;
+    private double payRate;
+
+    //ConsTructor
+    public Employee(int employeeID, String name, double hoursWorked, double payRate) {
+        this.employeeID = employeeID;
+        this.name = name;
+        this.hoursWorked = hoursWorked;
+        this.payRate = payRate;
+    }
+
+    /
+
+
+}
